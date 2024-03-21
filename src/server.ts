e95d25e96b5d83916c17438b3e4b5a7c3562c472
Neves-Rafael <nevesrafael.dev@gmail.com>
2024-03-20 22:47:49 -0300

    :sparkles: Feat metrics route

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import fastify from "fastify";
 import { z } from "zod"
 import { sql } from "./lib/postgres";
 import postgres from "postgres";
+import { redis } from "./lib/redis";
 
 const app = fastify();
 
@@ -24,6 +25,7 @@ app.get("/:code", async (request, reply) => {
 
   const link = result[0];
 
+  await redis.zIncrBy("metrics", 1, String(link.id))
 
   return reply.redirect(301, link.original_url)
 
@@ -72,6 +74,21 @@ app.post("/api/links", async (request, reply)=>{
     return reply.status(500).send({ message: "Internal error" })
   }
 
+})
+
+app.get("/api/metrics", async () => {
+  const result = await redis.zRangeByScoreWithScores("metrics", 0, 50)
+
+  const metrics = result
+  .sort((a,b) => b.score - a.score)
+  .map(item => {
+    return{
+      shortLinkId: Number(item.value),
+      clicks: item.score
+    }
+  })
+
+  return metrics
 })
 
 app.listen({
